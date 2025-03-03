9bfe198f8f60d658b86ccfb0d3ef4a290c06b978
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-03-04 02:39:10 +0300

    Create donguler06.php

---
+++ dongüler/donguler06.php
@@ -0,0 +1,21 @@
+<?php
+
+$sporlar = array(
+    "ToplaOynananlar" => array
+    (
+        "Futbol" => array(
+            "Orta saha",
+            "Forvet",
+            "Defans"
+        ),
+        2 => "Basketbol",
+        3 => "Tenis"
+    ),
+    "Atletizm" => array
+    (
+        4 => "Koşu Yarışı",
+        5 => "Uzun Atlama"
+    )
+);
+
+print_r($sporlar);
