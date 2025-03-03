290b71f5eee4cb1833c83e185d28dd8b5d3c211a
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-03-03 03:36:34 +0300

    Create degiskenler08.php

---
+++ degiskenler/degiskenler08.php
@@ -0,0 +1,16 @@
+<?php
+
+$renk = "siyah";
+switch ($renk) {
+    case "kırmız":
+        echo 'seçilen renk kırmız';
+        break;
+    case "mavi":
+        echo 'seçilen renk mavi';
+        break;
+    case "yeşil":
+        echo 'seçilen renk yeşil';
+        break;
+    default:    
+    echo 'renk seçimi hatalı yapıldı!';
+}
