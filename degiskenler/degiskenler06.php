845bcd00135d3a2542557b3e546d37f7099a4750
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-03-03 03:36:20 +0300

    Create degiskenler06.php

---
+++ degiskenler/degiskenler06.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+== : eşittir
+!= : eşit değildir
+<> : eşit değildir
+<  : küçüktür
+>  : büyüktür
+<= : küçük eşittir
+>= : büyük eşittir
+*/
+
+$a = 10;
+$b = 15;
+
+//var_dump($a == $b); // a değişkeni b'ye eşit mi 
+
+//var_dump($a <> $b); // a değişkeni b'ye eşit mi 
+
+var_dump($a > $b); // a değişkeni b'ye eşit mi 
