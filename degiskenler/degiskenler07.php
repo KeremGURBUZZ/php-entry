2f121b5f9f4d6e9ef2311ce708b0404848fcbfbd
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-03-03 03:36:27 +0300

    Create degiskenler07.php

---
+++ degiskenler/degiskenler07.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+== : eşittir
+!= : eşit değildir
+<> : eşit değildir
+<  : küçüktür
+>  : büyüktür
+<= : küçük eşittir
+>= : büyük eşittir
+
+&& : ve
+|| : veya
+*/
+
+$a = 10;
+$b =15;
+
+if ($a == 8)
+{
+    //koşul doğruysa çalışacak 
+    echo 'doğru';
+}
+else if ($a ==10)
+{
+    echo 'a değişkeni = 10';
+}
+else 
+{
+    // koşul yanlışsa çalışacak
+    echo 'yanlış';
+}
