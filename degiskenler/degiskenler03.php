0d7771c852edef5918065f19c80c8f28f89d72ac
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-03-03 03:36:02 +0300

    Create degiskenler03.php

---
+++ degiskenler/degiskenler03.php
@@ -0,0 +1,16 @@
+<?php
+
+$x = 15;
+
+//TEK TIRNAK
+
+//echo '$x bir sayıdır.';
+//echo 'Ankara\'ya nasıl gidebilirim?';
+//echo 'deneme " deneme';
+
+
+//ÇİFT TIRNAK
+
+//echo "$x";
+//echo "Ankara'ya nasıl gidebilirim?";
+//echo "deneme " deneme";
