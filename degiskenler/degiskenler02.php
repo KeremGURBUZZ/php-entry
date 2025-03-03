136f9d5bde52374d7d49f1290eac519999f20ab8
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-03-03 03:35:56 +0300

    Create degiskenler02.php

---
+++ degiskenler/degiskenler02.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+  VERİ TİPLERİ
+
+  integer: tam sayılar 10-20-150
+  float:ondalıklı sayılar 10.25-20.75
+  string:"düz yazılar" "aa"
+  boolean:mantıksal ifadeler true/false
+  array:diziler
+  object:nesneler
+*/
+
+$renkler = array("kırmızı", "mavi", "beyaz",4);
+var_dump($renkler);
