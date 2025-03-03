43f743bed971d8e435d9206154882d39f67fdfcb
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-03-03 03:36:14 +0300

    Create degiskenler05.php

---
+++ degiskenler/degiskenler05.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Dosyanın yolu:" . __FILE__ . "<br>";
+echo "Satır: " . __LINE__ . "<br>";
+echo "PHP Versiyon: " . PHP_VERSION . "<br>";
+echo "Sistem" . PHP_OS . "<br>";
+echo "Dizin: " . __DIR__ . "<br>";
