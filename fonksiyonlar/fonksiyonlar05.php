2ef08aa4d9823dd9cfae5c95f4b285bbe70d33f7
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-03-07 19:21:24 +0300

    Create fonksiyonlar05.php

---
+++ fonksiyonlar/fonksiyonlar05.php
@@ -0,0 +1,21 @@
+<?php
+
+$sayi1 = 10;
+$sayi2 = 25.4999;
+
+//echo floor($sayi2);
+//echo ceil($sayi2);
+echo round($sayi2);
+//echo tan/cot/sin/cos($sayi1);
+
+//echo rand(111,999);   random sayı üretmek için kullanılır     önemli***
+
+//echo sqrt(25);  kare kök almaya yarar
+
+//echo pow(12,4);   üs almaya yarar
+
+
+//echo abs(-48);   mutlak değer
+
+//echo max(10,15,20,25,33,444,555);   girilen sayıların en büyüğü
+//echo min(10,15,20,25,33,444,555);   girilen sayıların en küçüğünü yazdırır
