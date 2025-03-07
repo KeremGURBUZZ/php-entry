cb98845aef5c65b45657d1ef38158b108aa7a40b
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-03-07 19:51:44 +0300

    Create fonksiyonlar07.php

---
+++ fonksiyonlar/fonksiyonlar07.php
@@ -0,0 +1,25 @@
+<?php
+
+$renkler = array("mavi", "pembe", "yeşil", "sarı", "mor");
+$yaslar = array("mavi", "siyah", "yeşil", "turuncu", "mor");
+
+//var_dump($renkler);
+
+echo count($yaslar);
+
+//var_dump(is_array($renkler));
+
+//array_push($renkler, "birinci", "ikinci");
+
+//array_pop($renkler);
+
+//if (in_array("siyah", $renkler)) {
+//    echo 'var';
+//} else {
+//    echo 'yok';
+//}
+
+//$sonuc = array_diff($renkler, $yaslar);
+//print_r($sonuc);
+
+//var_dump($renkler);
