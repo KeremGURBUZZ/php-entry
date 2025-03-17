13f8e0cbbec9142d0687e984678712c9d1269d48
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-03-18 02:47:27 +0300

    Create include.php

---
+++ include/include.php
@@ -0,0 +1,12 @@
+<?php
+
+echo 'Burası include sayfasıdır';
+echo "<br>";
+
+include 'yedek.php';
+//include_once 'yedek.php';
+include 'yedek.php';
+include 'yedek.php';
+include 'yedek.php';
+include 'yedek.php';
+?>
