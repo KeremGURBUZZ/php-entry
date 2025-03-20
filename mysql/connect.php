d4323fbea26f23f57158a29e6338d98a132b89a3
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-03-21 00:31:55 +0300

    Update connect.php

--- mysql/connect.php
+++ mysql/connect.php
@@ -1,8 +1,16 @@
 <?php
-require_once 'functions.php';
+require_once 'functions.php';  
 
+$baslik = "Bu bir başlıktır";
+$aciklama = "Bu bir açıklamadır";
+//VERİTABANI EKLEME İŞLEMLERİ
 
-
-
-
+$insert = $db->prepare("INSERT INTO `haberler` (`baslik`, `aciklama`) VALUES (:baslik, :aciklama)");
+$insert->bindValue(":baslik", $baslik, PDO::PARAM_STR);
+$insert->bindValue(":aciklama", $aciklama, PDO::PARAM_STR);
+if($insert->execute()){
+    echo "Başarıyla eklendi";
+}else {
+    echo "Hata oluştu";
+}
 ?>
